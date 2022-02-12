adf5ec26b5eca336f8c4b1919ca9cef988e5229c
Vladimir Fidunin <vladimir.fidunin@lamoda.ru>
2022-02-12 18:57:47 +0300

    Add single key layer switch

--- keyboards/lily58/keymaps/w33dw0r7d/config.h
+++ keyboards/lily58/keymaps/w33dw0r7d/config.h
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
 
 /* Encoder support */
 #undef ENCODERS_PAD_A
